b3f405b021ac5bd828d8185e818eb0aedc0d6c15
FF <focused.fault@gmail.com>
2023-07-19 20:51:15 -0400

    1.0.2

--- CoolerEclipse/CoolerEclipse.cs
+++ CoolerEclipse/CoolerEclipse.cs
@@ -9,13 +9,27 @@ using System.Linq;
 
 namespace CoolerEclipse
 {
-  [BepInPlugin("com.Nuxlar.CoolerEclipse", "CoolerEclipse", "1.0.1")]
+  [BepInPlugin("com.Nuxlar.CoolerEclipse", "CoolerEclipse", "1.0.2")]
 
   public class CoolerEclipse : BaseUnityPlugin
   {
     GameObject eclipseWeather = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/eclipseworld/Weather, Eclipse.prefab").WaitForCompletion();
     public static ConfigEntry<bool> shouldBeChance;
     private static ConfigFile CEConfig { get; set; }
+
+    private string[] whitelistedMaps = new string[] {
+      "blackbeach2",
+      "golemplains",
+      "golemplains2",
+      "goolake",
+      "ancientloft",
+      "frozenwall",
+      "wispgraveyard",
+      "sulfurpools",
+      "FBLScene",
+      "shipgraveyard",
+      "rootjungle"
+};
 
     public void Awake()
     {
@@ -29,19 +43,37 @@ namespace CoolerEclipse
     {
       GameObject[] gameObjects = GameObject.FindObjectsOfType<GameObject>();
       string sceneName = SceneManager.GetActiveScene().name;
-      if (gameObjects.Where((obj) => obj.name.Contains("Weather,")).Count() > 0 && Run.instance.selectedDifficulty >= DifficultyIndex.Eclipse1 && sceneName != "voidraid" && sceneName != "voidstage")
+      if (Run.instance.selectedDifficulty >= DifficultyIndex.Eclipse1 && whitelistedMaps.Contains(sceneName))
       {
         if (shouldBeChance.Value && Random.value > 0.5)
         {
           orig(self);
           return;
         }
+
+        if (gameObjects.Where((obj) => obj.name.Contains("Weather,")).Count() > 0)
+        {
+          gameObjects.Where((obj) => obj.name.Contains("Weather,")).First<GameObject>().SetActive(false);
+          if (gameObjects.Where((obj) => obj.name == "Sun").Count() > 0)
+            gameObjects.Where((obj) => obj.name == "Sun").First<GameObject>().SetActive(false);
+        }
+        else
+          GameObject.Find("Directional Light (SUN)").gameObject.SetActive(false);
+
+        if (sceneName == "rootjungle")
+          GameObject.Find("HOLDER: Weather Set 1").gameObject.SetActive(false);
+
         GameObject newWeather = Instantiate(eclipseWeather, new Vector3(0, 0, 0), Quaternion.identity);
         newWeather.transform.GetChild(3).GetChild(2).gameObject.SetActive(true);
+
+        if (sceneName == "FBLScene" || sceneName == "shipgraveyard")
+          newWeather.transform.GetChild(1).GetComponent<Light>().intensity = 1;
+        if (sceneName == "frozenwall")
+        {
+          newWeather.transform.GetChild(0).gameObject.SetActive(false);
+          newWeather.transform.GetChild(1).gameObject.SetActive(false);
+        }
         NetworkServer.Spawn(newWeather);
-        gameObjects.Where((obj) => obj.name.Contains("Weather,")).First<GameObject>().SetActive(false);
-        if (gameObjects.Where((obj) => obj.name == "Sun").Count() > 0)
-          gameObjects.Where((obj) => obj.name == "Sun").First<GameObject>().SetActive(false);
       }
       orig(self);
     }
--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 Changes the skybox and weather on specific stages when playing Eclipse. There's a config for making the change a 50% chance instead of guaranteed.
 
-Affected Maps: Titanic Plains 1 & 2, Aphelian Sanctuary, Abandoned Acqueduct, Scorched Acres, Siren's Call
+Affected Maps: Distant Roost 2, Titanic Plains 1 & 2, Aphelian Sanctuary, Abandoned Acqueduct, Scorched Acres, Rallypoint Delta, Sulfur Pools, Fogbound Lagoon (if installed), Sundered Grove, Siren's Call
 
 ## Changelog
+
+**1.0.2**
+
+- Added more maps
+- Tweaks on darker maps (siren's and FBL)
+- Added fogbound support
 
 **1.0.1**
 
